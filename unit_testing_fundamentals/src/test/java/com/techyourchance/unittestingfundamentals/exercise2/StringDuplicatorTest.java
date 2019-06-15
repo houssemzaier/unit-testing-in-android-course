155352b3f5179ca1ac03f72544a7d2385dcc29f7
houssemzaier <houssem.zaier@gmail.com>
2019-06-15 18:03:03 +0200

    [section1] add tests and fix bug for the StringDuplicator

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StringDuplicatorTest {
+    private StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_az_azaz() {
+        String result = SUT.duplicate("az");
+        assertThat(result, is("azaz"));
+    }
+
+    @Test
+    public void duplicate_emptyCharacter_empty() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_whitespace_doubleWhiteSpace() {
+        String result = SUT.duplicate(" ");
+        assertThat(result, is("  "));
+    }
 }
