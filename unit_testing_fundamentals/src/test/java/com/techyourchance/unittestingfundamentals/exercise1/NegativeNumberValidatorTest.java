28bba576a8add602c6208fde8c34adb005b406a4
houssemzaier <houssem.zaier@gmail.com>
2019-06-15 16:19:15 +0200

    [section1] add tests and fix bug for the NegativeNumberValidator

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class NegativeNumberValidatorTest {
+    private NegativeNumberValidator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void isNegative_negativeNumber_true() {
+        boolean result = SUT.isNegative(-5);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void isNegative_positiveNumber_false() {
+        boolean result = SUT.isNegative(5);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void isNegative_zero_false() {
+        boolean result = SUT.isNegative(0);
+        assertThat(result, is(false));
+    }
 
 }
